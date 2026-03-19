bc538aa9ac884c2fcca0461086164cac6ae1a5ac
34j <34j.github@proton.me>
2026-03-19 12:01:39 +0900

    chore: initial commit

---
+++ src/road_traffic_act/__main__.py
@@ -0,0 +1,5 @@
+"""Make the CLI runnable using python -m road_traffic_act."""
+
+from .cli import app
+
+app(prog_name="road-traffic-act")
---
+++ tests/test_main.py
@@ -0,0 +1,6 @@
+from road_traffic_act.main import add
+
+
+def test_add():
+    """Adding two number works as expected."""
+    assert add(1, 1) == 2
